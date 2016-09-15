fad8990d7312cca9b9d28924f1596c3d4baf3a35
David Chen <darkbaby123@gmail.com>
2016-09-15 23:10:37 +0800

    add solution 2

---
+++ test/solution_2_spec.js
@@ -0,0 +1,6 @@
+const sumOfDivided = require('../lib/solution_2')
+const sumOfDividedExample = require('./sum_of_divided_example')
+
+describe('solution 2', () => {
+  sumOfDividedExample(sumOfDivided)
+})
